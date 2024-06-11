0f54a08c227576b3ec294e01f630df5d6772a378
Keiliel Oliveira <keilieloliveira.2004@gmail.com>
2024-06-11 14:33:26 -0300

    V 2.0.0
    Sistema finalizado por hora.

--- index.php
+++ index.php
@@ -12,57 +12,73 @@ require 'vendor/autoload.php';
 
 $router = new Router\Router;
 
-$router->globalMiddlewares('after', [function(Request $req, Response $res) {
-    $res->setContent("Conteudo do after middleware global<br>");
+$router->globalMiddlewares('before', [function(Request $req, Response $res) {
+
+    $requestMethod = $req->requestMethod();
+    $url = $req->url();
+    $userId = $req->requestId();
+
+    $res->setBeforeContent("Sobre a requisição<br>");
+    $res->setBeforeContent("Metodo da requisição: $requestMethod<br>");
+    $res->setBeforeContent("Url da requisição: $url<br>");
+    $res->setBeforeContent("ID do usuario: $userId<br>");
 }]);
 
 $router->get('/', function(RouteMethods $route) {
 
+    $route->params(['name' => 'Keiliel Oliveira']);
     $route->controller(function(Request $req, Response $res) {
-        $url = $req->url();
-        $res->setContent("URL: $url<br>Token: {csrf_token}<br>");
-    });
 
+        $name = $req->params('name');
+        $res->setContent("Bem vindo <b>$name</b>!<br><br>");
+    });
     $route->beforeMiddlewares(function(Request $req, Response $res) {
-        $res->setContent("Conteudo do before middleware da rota.<br>");
+        $token = $req->csrfToken();
+        $res->setContent("Token CSRF: $token<br><br>");
     });
-
     $route->afterMiddlewares(function(Request $req, Response $res) {
-        $res->setContent("Conteudo do after middleware da rota.<br>");
+        $queryParams = $req->queryParams();
+        $urlParams = $req->urlHiddenParams();
+        $params = $req->params();
+
+        $res->setAfterContent("Sobre os parametros da rota");
+        $res->setAfterContent("<br>Parametros GET: " . count($queryParams));
+        $res->setAfterContent("<br>Parametros da url: " . count($urlParams));
+        $res->setAfterContent("<br>Parametros da rota: " . count($params));
     });
 
 });
 
-$router->group('/{user}', function(RouteGroup $group) {
-
-    $group->get('/{id:[0-9]{8}}', function(RouteMethods $route) {
-
-        $route->controller(function(Request $req, Response $res) {
-            $url = $req->url();
-            $res->setContent("URL: $url<br>Token: {csrf_token}<br>");
-        });
-
-    });
+$router->group('/{page}', function(RouteGroup $group) {
 
     $group->beforeGroupMiddlewares(function(Request $req, Response $res) {
-        $res->setContent("Conteudo do before middleware do grupo.<br>");
+        $res->setContent("Conteudo do before middleware do grupo.<br><br>");
     });
-    
-    $group->afterGroupMiddlewares(function(Request $req, Response $res) {
-        $user = $req->urlHiddenParams('user');
-        $id = $req->urlHiddenParams('id');
 
-        $res->setContent("Bem vindo <b>$user</b><br>ID: $id<br>");
+    $group->afterGroupMiddlewares(function(Request $req, Response $res) {
+        $res->setContent("Conteudo do after middleware do grupo.");
     });
+
+    $group->get('', function(RouteMethods $route) {
+        
+        $route->params(['name' => 'Keiliel Oliveira']);
+        $route->controller(function(Request $req, Response $res) {
+
+            $name = $req->params('name');
+            $page = $req->urlHiddenParams('page');
+            $res->setContent("Bem vindo a pagina <b>$page $name</b>!<br>");
+        });
+        $route->afterMiddlewares(function(Request $req, Response $res) {
+            $res->setContent("Token: {csrf_token}<br><br>");
+        });
+    });
+
 });
 
-$router->globalMiddlewares('before', [function(Request $req, Response $res) {
-    $res->setContent("Conteudo do before middleware global<br>");
+$router->globalMiddlewares('after', [function(Request $req, Response $res) {
+    $res->setAfterContent("<br>Rota executada com sucesso!");
 }]);
 
 $router->handle();
-
-$debug = new RouterDebug;
-$debug->routes(false);
 
 ?>
